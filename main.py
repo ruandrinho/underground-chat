7f1695904c38c4da62e6f2feb67ee415456bf4d6
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-09-19 12:26:00 +0500

    Read and print chat messages

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import asyncio
 
+
+async def read_chat():
+    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
+    while not reader.at_eof():
+        data = await reader.readline()
+        print(data.decode())
+
+asyncio.run(read_chat())
