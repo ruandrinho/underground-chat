260b1d301daf0e5a3e4e4b689f93e534c9fecbf9
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-09-27 12:12:46 +0500

    Add reading messages from chat

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import logging
 import gui
 import time
 
+import aiofiles
 from environs import Env
+
+from minechat_utils import get_minechat_connection
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +16,20 @@ async def generate_msgs(queue):
     while True:
         queue.put_nowait(round(time.time()))
         await asyncio.sleep(1)
+
+
+async def read_msgs(host, port, queue):
+    async with (
+        # aiofiles.open(history_file, 'a') as chatfile,
+        get_minechat_connection(host, port) as (reader, writer)
+    ):
+        while not reader.at_eof():
+            message = await reader.readline()
+            queue.put_nowait(message.decode().strip())
+            # now = datetime.datetime.now()
+            # message_with_datetime = f'[{now.strftime("%d.%m.%y %H:%M")}] {message.decode()}'
+            # print(message_with_datetime, end='')
+            # await chatfile.write(message_with_datetime)
 
 
 async def main():
@@ -33,7 +50,7 @@ async def main():
     parser.add_argument('--historyfile', '-f', help='File for history')
     args = parser.parse_args()
 
-    minechat_config = {
+    config = {
         'host': args.host if args.host else env('HOST', default='minechat.dvmn.org'),
         'reading_port': args.readingport if args.readingport else env.int('READING_PORT', default=5000),
         'writing_port': args.writingport if args.writingport else env.int('WRITING_PORT', default=5050),
@@ -50,7 +67,7 @@ async def main():
     loop.run_until_complete(
         await asyncio.gather(
             gui.draw(messages_queue, sending_queue, status_updates_queue),
-            generate_msgs(messages_queue)
+            read_msgs(config['host'], config['reading_port'], messages_queue)
         )
     )
 
